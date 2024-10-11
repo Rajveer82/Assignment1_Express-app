423598119d49abe7b7c399eebef4a133cfd7a67b
Rajveer <gillrajveer487@gmail.com>
2024-10-10 21:17:07 -0400

    Added connection string

--- Lab3Expressapp/index.js
+++ Lab3Expressapp/index.js
@@ -1,10 +1,16 @@
 //import express
 const express = require(`express`);
 const bodyParser = require(`bodyParser`);
+const mongoose = require(`mongoose`);
 
 
 //Initialize the express app
 const app = express();
+
+//MongoDb Atlas connection String
+const mongoURI = 'mongodb+srv://Rajveer:Rajjo123@cluster0.jdl76.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+
 
 //Middleware to parseJSON bodies
 app.use(bodyParser.json());
