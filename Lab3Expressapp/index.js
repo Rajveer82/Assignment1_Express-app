5b555793e140070f4a2bed05e82441a6cf9a89bc
Rajveer <gillrajveer487@gmail.com>
2024-10-10 22:08:07 -0400

    Added post end point

--- Lab3Expressapp/index.js
+++ Lab3Expressapp/index.js
@@ -1,7 +1,7 @@
 //import express
 const express = require(`express`);
-const bodyParser = require(`bodyParser`);
-const mongoose = require(`mongoose`);
+const bodyParser = require(`body-parser`);
+const mongoose = require(`mongoose`); 
 
 
 //Initialize the express app
@@ -10,6 +10,14 @@ const app = express();
 //MongoDb Atlas connection String
 const mongoURI = 'mongodb+srv://Rajveer:Rajjo123@cluster0.jdl76.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
+//connect to mongoDb Atlas
+mongoose.connect(mongoURI)
+ .then(()=>{
+    console.log(`Connected to mongoDb`);
+ })   
+.catch((error)=>{
+    console.error(`Error connecting to mongoDb`,error);
+});
 
 
 //Middleware to parseJSON bodies
@@ -19,6 +27,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 // Define a root route
 app.get('/', (req, res) => {
     res.send('Welcome to the first program of Node.js Express');
+});
+
+app.post(`/submit`,(req,res)=>{
+    res.send(`Received data:${req.body.data}`)
 });
 
 //set the port
