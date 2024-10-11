c71b5feb65e2a7b73a6404df83ca4c90191a0d10
Rajveer <gillrajveer487@gmail.com>
2024-10-11 16:32:26 -0400

    Added changes in routes.js

--- Lab4/src/routes/routes.js
+++ Lab4/src/routes/routes.js
@@ -1,29 +1,18 @@
 const express = require('express');
+const { getMovies, createMovie, getMovieById, updateMovie, deleteMovie, importMovies } = require('../controllers/controller');
+
 const router = express.Router();
-const movieController = require('../controllers/controller');
-const {validateMovie}=require('../middleware/middleware');
+//Route to get all movies with search and filter functionality
+router.get('/movies', getMovies);
+
+// other routes for creating, updating, deleting movies
+router.post('/movies', createMovie);
+router.get('/movies/:id', getMovieById);
+router.put('/movies/:id', updateMovie);
+router.delete('/movies/:id', deleteMovie);
 
 //Route to import movies
-router.post('/import',movieController.importMovies);
-
-//Route to get all movies
-router.get('/',movieController.getMovies);
-
-//Route to get a singlre movie by id
-router.get('/:id',movieController.getMovieById);
-
-//Route to update a movie by id
-
-router.put('/update/:id', validateMovie,movieController.updateMovie);
-
-//Route top delete a movie by id
-
-router.delete('/delete/:id',movieController.deleteMovie);
-
-
-//Route to create a new movie
-//Validate movie before creating
-router.post('/create',validateMovie,movieController.createMovie);
+router.post('/movies/import', importMovies);
 
 
 module.exports = router;
