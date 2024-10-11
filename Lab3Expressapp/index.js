c115eb391ffa3f5fc1145a377b66bad3f1c7431a
Rajveer <gillrajveer487@gmail.com>
2024-10-10 21:07:50 -0400

    Added bodyParser to the main file

--- Lab3Expressapp/index.js
+++ Lab3Expressapp/index.js
@@ -1,8 +1,19 @@
 //import express
 const express = require(`express`);
+const bodyParser = require(`bodyParser`);
+
 
 //Initialize the express app
 const app = express();
+
+//Middleware to parseJSON bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+// Define a root route
+app.get('/', (req, res) => {
+    res.send('Welcome to the first program of Node.js Express');
+});
 
 //set the port
 const port = 3000;
