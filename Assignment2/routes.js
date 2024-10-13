0d7aa07acd51892a296dfa76334502263b49f694
Rajveer <gillrajveer487@gmail.com>
2024-10-13 09:59:30 -0400

    new route for user api

--- Assignment2/routes.js
+++ Assignment2/routes.js
@@ -5,6 +5,7 @@
 var express = require('express');
 var { topRecipe, getRecipeList, findRecipe, addRecipe, updateRecipe, deleteRecipe } = require("./controller/recipe");
 var router = express.Router();
+const { createUser, login,logout } = require('./controller/user');
 // Set up the route
 var setRoute = function (){
     router.get('/getTopRecipe', topRecipe)
@@ -13,6 +14,12 @@ var setRoute = function (){
     router.post('/recipe/add', addRecipe)
     router.put('/recipe/update/:id', updateRecipe)
     router.delete('/recipe/delete/:id', deleteRecipe)
+
+    
+    // user routes
+    router.post('/user/create', createUser)
+    router.post('/user/login', login)
+    router.get('/user/logout', logout)
     return router
 }
 module.exports = {setRoute}
