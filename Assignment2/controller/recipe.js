8de90fddce6e79e758b3f573e83c20ebd6e5b36a
Rajveer <gillrajveer487@gmail.com>
2024-10-13 09:31:05 -0400

    find recipe logic and list receipe route

--- Assignment2/controller/recipe.js
+++ Assignment2/controller/recipe.js
@@ -4,11 +4,11 @@
 // Date: 29 September 2024
 var Recipe = require("../models/Recipe");
 // Function to get the top 20 recipes sorted by average rating
-var topRecipe = async function (req, res){
+var topRecipe = async function (req, res) {
     try {
         const list = await Recipe
             .find() // Find all recipes
-            .sort({averageRating: -1}) // Sort by averageRating in descending order
+            .sort({ averageRating: -1 }) // Sort by averageRating in descending order
             .limit(20) // Limit the result to 20 recipes
         res.send(list) // Send the list of recipes as the response
     } catch (err) {
@@ -23,7 +23,17 @@ var getRecipeList = async function (req, res) {
         res.send(err)
     }
 }
+var findRecipe = async function (req, res) {
+    try {
+        const recipe = await Recipe.findById(req.params.id)
+        res.send(recipe)
+    } catch (err) {
+        res.send(err)
+    }
+}
 module.exports = {
     topRecipe,
-    getRecipeList
+    getRecipeList,
+    findRecipe
 }
+
--- Assignment2/routes.js
+++ Assignment2/routes.js
@@ -3,11 +3,12 @@
 // Student Id: 200546782
 // Date: 29 September 2024
 var express = require('express');
-var {topRecipe} = require("./controller/recipe");
+var { topRecipe, getRecipeList } = require("./controller/recipe");
 var router = express.Router();
 // Set up the route
 var setRoute = function (){
     router.get('/getTopRecipe', topRecipe)
+    router.get('/recipe/list', getRecipeList)
     return router
 }
 module.exports = {setRoute}
