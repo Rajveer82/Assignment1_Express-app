83fd97982d0998210bd77858e9eef2114a17c62c
Rajveer <gillrajveer487@gmail.com>
2024-10-13 09:33:13 -0400

    API find recipe id

--- Assignment2/routes.js
+++ Assignment2/routes.js
@@ -3,12 +3,13 @@
 // Student Id: 200546782
 // Date: 29 September 2024
 var express = require('express');
-var { topRecipe, getRecipeList } = require("./controller/recipe");
+var { topRecipe, getRecipeList, findRecipe } = require("./controller/recipe");
 var router = express.Router();
 // Set up the route
 var setRoute = function (){
     router.get('/getTopRecipe', topRecipe)
     router.get('/recipe/list', getRecipeList)
+    router.get('/recipe/find/:id', findRecipe)
     return router
 }
 module.exports = {setRoute}
