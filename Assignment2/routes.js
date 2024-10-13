33739a23c1bf5dbc857102ca5fb2907d2ed86c8a
Rajveer <gillrajveer487@gmail.com>
2024-10-13 09:47:06 -0400

    API to update recipe

--- Assignment2/routes.js
+++ Assignment2/routes.js
@@ -3,7 +3,7 @@
 // Student Id: 200546782
 // Date: 29 September 2024
 var express = require('express');
-var { topRecipe, getRecipeList, findRecipe, addRecipe } = require("./controller/recipe");
+var { topRecipe, getRecipeList, findRecipe, addRecipe, updateRecipe } = require("./controller/recipe");
 var router = express.Router();
 // Set up the route
 var setRoute = function (){
@@ -11,6 +11,7 @@ var setRoute = function (){
     router.get('/recipe/list', getRecipeList)
     router.get('/recipe/find/:id', findRecipe)
     router.post('/recipe/add', addRecipe)
+    router.put('/recipe/update/:id', updateRecipe)
     return router
 }
 module.exports = {setRoute}
